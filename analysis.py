9ed1c4ea24df3675a47d54ccdcfc0406dbefd4a2
FaolanH <faolanash01@gmail.com>
2025-05-06 18:25:42 +0100

    created four new txt. files and a source (W3 Schools) for how this was done

--- analysis.py
+++ analysis.py
@@ -2,3 +2,27 @@
 # Project: Analyse the Iris dataset and discuss different ways of presenting the data
 # Author: Faolán Hamilton
 
+# ---------- Creating initial files for each variable ---------- 
+# Source for writing and creating txt files, W3 Schools: https://w3schools.tech/tutorial/python/python_write_to_file
+
+# Sepal length
+sl_file = open("sepal_length_file.txt", "w")
+sl_file.write ("This file is a summary of the sepal length data from the Iris dataset.")
+sl_file.close()
+
+# Sepal width
+sl_file = open("sepal_width_file.txt", "w")
+sl_file.write ("This file is a summary of the sepal width data from the Iris dataset.")
+sl_file.close()
+
+# Petal length
+sl_file = open("petal_length_file.txt", "w")
+sl_file.write ("This file is a summary of the petal length data from the Iris dataset.")
+sl_file.close()
+
+# Petal width
+sl_file = open("petal_width_file.txt", "w")
+sl_file.write ("This file is a summary of the petal width data from the Iris dataset.")
+sl_file.close()
+
+# ----------
