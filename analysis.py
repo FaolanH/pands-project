64ff63f55d3b9104a963fbabf5396d4db97d20a1
FaolanH <faolanash01@gmail.com>
2025-05-06 18:41:37 +0100

    importing the modules I will need for the project so far, may need to import more at a later stage

--- analysis.py
+++ analysis.py
@@ -2,6 +2,21 @@
 # Project: Analyse the Iris dataset and discuss different ways of presenting the data
 # Author: Faolán Hamilton
 
+# ---------- Importing the modules I will need for this project ----------
+
+# this module is used to analyse the data and perform calculations
+import numpy as np 
+
+# this module is used to create a DataFrame of the dataset
+import pandas as pd 
+
+# this module is used to visualise the data on plots
+import matplotlib.pyplot as plt 
+
+# this module is used to visualise the data on more complicated plots
+import seaborn as sns 
+
+
 # ---------- Creating an initial summary file for the variables ---------- 
 # Source for writing and creating txt files, W3 Schools: https://w3schools.tech/tutorial/python/python_write_to_file
 
