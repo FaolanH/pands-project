0e844951aa146e5442d23507fa08e568b91eb819
FaolanH <faolanash01@gmail.com>
2025-05-06 18:36:25 +0100

    update the file to have only one file created, not four

--- analysis.py
+++ analysis.py
@@ -2,27 +2,12 @@
 # Project: Analyse the Iris dataset and discuss different ways of presenting the data
 # Author: Faolán Hamilton
 
-# ---------- Creating initial files for each variable ---------- 
+# ---------- Creating an initial summary file for the variables ---------- 
 # Source for writing and creating txt files, W3 Schools: https://w3schools.tech/tutorial/python/python_write_to_file
 
-# Sepal length
-sl_file = open("sepal_length_file.txt", "w")
-sl_file.write ("This file is a summary of the sepal length data from the Iris dataset.")
-sl_file.close()
-
-# Sepal width
-sl_file = open("sepal_width_file.txt", "w")
-sl_file.write ("This file is a summary of the sepal width data from the Iris dataset.")
-sl_file.close()
-
-# Petal length
-sl_file = open("petal_length_file.txt", "w")
-sl_file.write ("This file is a summary of the petal length data from the Iris dataset.")
-sl_file.close()
-
-# Petal width
-sl_file = open("petal_width_file.txt", "w")
-sl_file.write ("This file is a summary of the petal width data from the Iris dataset.")
-sl_file.close()
+# Features_file
+features_file = open("features_file.txt", "w")
+features_file.write ("This file is a summary of the four features: sepal length, sepal width, petal length and petal width data from the Iris dataset.")
+features_file.close()
 
 # ----------
