7dfc887127e3762ae7dcce3e4c1c04a56ee4032f
FaolanH <faolanash01@gmail.com>
2025-05-07 20:49:14 +0100

    I brought in the load_iris dataset from sklearn.dataset, which allowed me to create a numpy array of the three species types using sepal width as an example and plot all three on the histogram to compare across

--- analysis.py
+++ analysis.py
@@ -24,6 +24,8 @@ from sklearn.datasets import load_iris
 
 pd_iris_data = pd.read_csv ("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv")
 
+# Had to bring in the iris_data with the species as code name as they would not accept creating an array with a string
+iris_data = load_iris()
 
 # ---------- Creating an initial summary file for the variables ---------- 
 # Source for writing and creating txt files, W3 Schools: https://w3schools.tech/tutorial/python/python_write_to_file
@@ -35,34 +37,21 @@ features_file.close()
 
 # ---------- Dividing the features up ----------
 # Source: DataCamp, Intermediate Python, Dictionaries and Pandas Chapter, loc and iloc (https://campus.datacamp.com/courses/intermediate-python/dictionaries-pandas?ex=17)
-
+'''
 sepal_length = pd_iris_data.iloc[0:,0]
 sepal_width = pd_iris_data.iloc[0:,1]
 petal_length = pd_iris_data.iloc[0:,2]
 petal_width = pd_iris_data.iloc[0:,3]
 
 # Ensure that the output doesn't include unnecessary data like dtype:float64 (https://stackoverflow.com/questions/74696163/how-to-remove-name-and-dtype-from-output-code)
-'''
+
 print (f'Sepal Length is:\n{sepal_length.to_string()}')
 print (f'Sepal Width is:\n{sepal_width.to_string()}')
 print (f'Petal Length is:\n{petal_length.to_string()}')
 print (f'Petal Width is:\n{petal_width.to_string()}')
 '''
 
-# ---------- Breaking down by species ----------
-
-setosa = pd_iris_data.iloc[0:50,]
-versicolor = pd_iris_data.iloc[50:100,]
-virginica = pd_iris_data.iloc[100:,]
-
-
-setosa_sw = pd_iris_data.iloc[0:50,1]
-versicolor_sw = pd_iris_data.iloc[50:100,1]
-virginica_sw = pd_iris_data.iloc[100:,1]
-
-sw = ([setosa_sw, versicolor_sw, virginica_sw])
-
-sepal_width_sw = np.array(setosa, pd_iris_data.iloc[0:,1])
+#epal_width_sw = np.array(setosa, pd_iris_data.iloc[0:,1])
 
 # ---------- Creating histograms for individual features ----------
 # Source: DataCamp, Intermediate Python, Chapter Matplotlib (https://campus.datacamp.com/courses/intermediate-python/matplotlib?ex=1)
@@ -92,18 +81,37 @@ plt.grid(linewidth = 0.5, c = 'grey', alpha = 0.2)
 
 # showing the plot
 plt.show()
+'''
 
+# Attempting to plot three bars on one histogram - I orginally attempted to do this with the pd_iris_data, but because the species name was a string not a float, I coudln't create the array I needed to plot all three on the one!
+# Source: https://matplotlib.org/stable/gallery/statistics/histogram_multihist.html
 
-# Attempting to plot three bars on one histogram
+# ---------- Breaking down by species ----------
 
-x = np.random.randn(1000, 3)
+setosa_sw = iris_data.data[0:50,1]
+versicolor_sw = iris_data.data[50:100,1]
+virginica_sw = iris_data.data[100:150,1]
+
+sw = ([setosa_sw, versicolor_sw, virginica_sw])
+
+# Setting the title
+plt.title("The sepal width of the Iris flower samples by species")
+
+# setting the x axis 
+plt.xlabel ("Sepal width (in centimeters)")
+
+# setting the y axis label
+plt.ylabel ("Number of samples per species")
+
+# setting the xticks to show the cm
+plt.xticks ([2.0,2.5,3.0,3.5,4.0,4.5], ["2cm", "2.5cm", "3cm", "3.5cm", "4cm", "4.5cm"])
 
 colours = ('#7c70cb', '#632AC5', '#B9B0E7')
 
+# setting a grid for context
+plt.grid(linewidth = 0.5, c = 'grey', alpha = 0.2)
+
 # plotting the histogram using sepal_length data, with 20 bins (or bars)
-#plt.hist (sepal_width_sw, bins = 20, edgecolor = 'black', color = colours)
+plt.hist (sw, bins = 10, edgecolor = 'black', color = colours)
 
-#plt.show()
-
-#print (sepal_width_sw)
-'''
+plt.show()
