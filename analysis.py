e6d1845b9df25eade899a45ea83bfc22764a6054
FaolanH <faolanash01@gmail.com>
2025-05-07 21:29:56 +0100

    Histograms: I went back to using the original pd_iris_data, and added a legend using the matplotlib module mpatches to set each species on the histogram. This is in a good place now, and ready to replicate across the other features

--- analysis.py
+++ analysis.py
@@ -19,6 +19,9 @@ import seaborn as sns
 # this module is used to bring in the dataset in a different form
 from sklearn.datasets import load_iris
 
+# bringing in mpatches allows the legend to be set easily
+import matplotlib.patches as mpatches
+
 
 # ---------- Importing the iris dataset through pandas, the species names can be called directly ----------
 
@@ -37,14 +40,14 @@ features_file.close()
 
 # ---------- Dividing the features up ----------
 # Source: DataCamp, Intermediate Python, Dictionaries and Pandas Chapter, loc and iloc (https://campus.datacamp.com/courses/intermediate-python/dictionaries-pandas?ex=17)
-'''
+
 sepal_length = pd_iris_data.iloc[0:,0]
 sepal_width = pd_iris_data.iloc[0:,1]
 petal_length = pd_iris_data.iloc[0:,2]
 petal_width = pd_iris_data.iloc[0:,3]
 
 # Ensure that the output doesn't include unnecessary data like dtype:float64 (https://stackoverflow.com/questions/74696163/how-to-remove-name-and-dtype-from-output-code)
-
+'''
 print (f'Sepal Length is:\n{sepal_length.to_string()}')
 print (f'Sepal Width is:\n{sepal_width.to_string()}')
 print (f'Petal Length is:\n{petal_length.to_string()}')
@@ -56,42 +59,29 @@ print (f'Petal Width is:\n{petal_width.to_string()}')
 # ---------- Creating histograms for individual features ----------
 # Source: DataCamp, Intermediate Python, Chapter Matplotlib (https://campus.datacamp.com/courses/intermediate-python/matplotlib?ex=1)
 
-# Sepal Length Histogram 
-'''
-# setting the colours
-colours = ('#7c70cb', '#632AC5', '#B9B0E7')
-
-# plotting the histogram using sepal_length data, with 20 bins (or bars)
-plt.hist (sw, bins = 20, edgecolor = 'black')#, color = colours)
-
-# setting the xticks to show the cm
-plt.xticks ([4.5,5.0,5.5,6.0,6.5,7.0,7.5,8.0], ["4.5cm", "5cm", "5.5cm", "6cm", "6.5cm", "7cm", "7.5cm", "8cm"])
-
-# Setting the title
-plt.title("The sepal length of the Iris flower samples")
-
-# setting the x axis 
-plt.xlabel ("Sepal length (in centimeters)")
-
-# setting the y axis label
-plt.ylabel ("Number of samples per bin")
-
-# setting a grid for context
-plt.grid(linewidth = 0.5, c = 'grey', alpha = 0.2)
-
-# showing the plot
-plt.show()
-'''
-
-# Attempting to plot three bars on one histogram - I orginally attempted to do this with the pd_iris_data, but because the species name was a string not a float, I coudln't create the array I needed to plot all three on the one!
+# Attempting to plot three bars on one histogram - I orginally attempted to do this with the pd_iris_data, but then thought because the species name was a string not a float, I coudln't create the array I needed to plot all three on the one! It turned out I didn't need to create a numpy array at all, and ended up not using that dataset for this.
 # Source: https://matplotlib.org/stable/gallery/statistics/histogram_multihist.html
+# Source: https://matplotlib.org/stable/users/explain/axes/legend_guide.html#controlling-the-legend-entries
 
 # ---------- Breaking down by species ----------
 
-setosa_sw = iris_data.data[0:50,1]
-versicolor_sw = iris_data.data[50:100,1]
-virginica_sw = iris_data.data[100:150,1]
+# to show the legend, this needs to be brought in as a sub plot
+fig,ax = plt.subplots()
+ 
+# Setting the legend styles for each variable
+setosa = mpatches.Patch(facecolor = '#7c70cb', edgecolor = 'black', label = 'Setosa')
+versicolor = mpatches.Patch(facecolor = '#632AC5', edgecolor = 'black', label = 'Versicolor')
+virginica =  mpatches.Patch(facecolor = '#B9B0E7', edgecolor = 'black', label = 'Virginica')
 
+# plotting the legend
+ax.legend(handles=[setosa, versicolor, virginica], loc = 'upper left', shadow = True)
+
+# Setting the data location for each of the species: 0:50 sets the first row (0) up to but not including 50 (aka 49) which is all the setosa data. The ,1 that follows sets the column (sepal width)
+setosa_sw = pd_iris_data.iloc[0:50,1]
+versicolor_sw = pd_iris_data.iloc[50:100,1]
+virginica_sw = pd_iris_data.iloc[100:150,1]
+
+# pulling all three 
 sw = ([setosa_sw, versicolor_sw, virginica_sw])
 
 # Setting the title
@@ -112,6 +102,6 @@ colours = ('#7c70cb', '#632AC5', '#B9B0E7')
 plt.grid(linewidth = 0.5, c = 'grey', alpha = 0.2)
 
 # plotting the histogram using sepal_length data, with 20 bins (or bars)
-plt.hist (sw, bins = 10, edgecolor = 'black', color = colours)
+plt.hist (sw, bins = 8, edgecolor = 'black', color = colours)
 
 plt.show()
