9f4c32f95580b9ddd5ffeb5dfc5c28f30dc043cf
FaolanH <faolanash01@gmail.com>
2025-05-11 23:45:13 +0100

    References: Sources have been set throughout the notebook, now I am adding in the sources at the bottom with references to the sources throughout

--- analysis.py
+++ analysis.py
@@ -2,7 +2,7 @@
 # Project: Analyse the Iris dataset and discuss different ways of presenting the data
 # Author: Faolán Hamilton
 
-# Source: https://stackoverflow.com/questions/8265583/dividing-python-module-into-multiple-regions
+# Source: 1) Setting regions
 
 # region Import
 
@@ -35,7 +35,7 @@ from matplotlib.colors import LinearSegmentedColormap, ListedColormap
 from tabulate import tabulate
 
 # ---------- Importing the iris dataset through pandas, the species names can be called directly ----------
-
+# Source: 2) Iris dataset
 pd_iris_data = pd.read_csv("https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv")
 
 # Had to bring in the iris_data with the species as code name as they would not accept creating an array with a string
@@ -46,44 +46,43 @@ iris_data = load_iris()
 # region Summary txt.
 
 # ---------- Creating an initial summary file for the variables ---------- 
-# Source for writing and creating txt files, W3 Schools: https://w3schools.tech/tutorial/python/python_write_to_file
+# Source for writing and creating txt files, W3 Schools: 3) Creating txt file
 
 #endregion
 
 # region Summary
-# ---------- Creating a DataFrame to show in the summary txt file ----------
-# Source: https://www.geeksforgeeks.org/how-to-make-a-table-in-python/#using-prettytable
+# ---------- Setting the data needed for the pdDataFrame ----------
 
 # Mean
-# Source: https://numpy.org/doc/stable/reference/generated/numpy.mean.html
+# Source: 4) np Mean
 sl_mean = np.mean(pd_iris_data['sepal_length'])
 sw_mean = np.mean(pd_iris_data['sepal_width'])
 pl_mean = np.mean(pd_iris_data['petal_length'])
 pw_mean = np.mean(pd_iris_data['petal_width'])
 
 # Median 
-# Source: https://numpy.org/doc/stable/reference/generated/numpy.median.html
+# Source: 5) np Median
 sl_median = np.median(pd_iris_data['sepal_length'])
 sw_median = np.median(pd_iris_data['sepal_width'])
 pl_median = np.median(pd_iris_data['petal_length'])
 pw_median = np.median(pd_iris_data['petal_width'])
 
 # Minimum 
-# Source: https://numpy.org/doc/stable//reference/generated/numpy.min.html
+# Source: 6) np Min 
 sl_min = np.min(pd_iris_data['sepal_length'])
 sw_min = np.min(pd_iris_data['sepal_width'])
 pl_min = np.min(pd_iris_data['petal_length'])
 pw_min = np.min(pd_iris_data['petal_width'])
 
 # Maximum 
-# Source: https://numpy.org/doc/stable//reference/generated/numpy.max.html
+# Source: 7) np Max
 sl_max = np.max(pd_iris_data['sepal_length'])
 sw_max = np.max(pd_iris_data['sepal_width'])
 pl_max = np.max(pd_iris_data['petal_length'])
 pw_max = np.max(pd_iris_data['petal_width'])
 
 # Standard Deviation
-# Source: https://numpy.org/doc/stable/reference/generated/numpy.std.html
+# Source: 8) Std Dev
 sl_stdd = np.std(pd_iris_data['sepal_length'])
 sw_stdd = np.std(pd_iris_data['sepal_width'])
 pl_stdd = np.std(pd_iris_data['petal_length'])
@@ -96,31 +95,36 @@ pw_stdd = np.std(pd_iris_data['petal_width'])
 # Summary by species
 # Setosa
 
-# Setosa Mean
+# Setosa Mean 
+# Source: 4) np Mean
 setosa_sl_mean = np.mean(pd_iris_data.iloc[0:50,0])
 setosa_sw_mean = np.mean(pd_iris_data.iloc[0:50,1])
 setosa_pl_mean = np.mean(pd_iris_data.iloc[0:50,2])
 setosa_pw_mean = np.mean(pd_iris_data.iloc[0:50,3])
 
 # Setosa Median 
+# Source: 5) np Median
 setosa_sl_median = np.median(pd_iris_data.iloc[0:50,0])
 setosa_sw_median = np.median(pd_iris_data.iloc[0:50,1])
 setosa_pl_median = np.median(pd_iris_data.iloc[0:50,2])
 setosa_pw_median = np.median(pd_iris_data.iloc[0:50,3])
 
 # Setosa Minimum 
+# Source: 6) np Min 
 setosa_sl_min = np.min(pd_iris_data.iloc[0:50,0])
 setosa_sw_min = np.min(pd_iris_data.iloc[0:50,1])
 setosa_pl_min = np.min(pd_iris_data.iloc[0:50,2])
 setosa_pw_min = np.min(pd_iris_data.iloc[0:50,3])
 
 # Setosa Maximum 
+# Source: 7) np Max
 setosa_sl_max = np.max(pd_iris_data.iloc[0:50,0])
 setosa_sw_max = np.max(pd_iris_data.iloc[0:50,1])
 setosa_pl_max = np.max(pd_iris_data.iloc[0:50,2])
 setosa_pw_max = np.max(pd_iris_data.iloc[0:50,3])
 
 # Setosa Standard Deviation
+# Source: 8) Std Dev
 setosa_sl_stdd = np.std(pd_iris_data.iloc[0:50,0])
 setosa_sw_stdd = np.std(pd_iris_data.iloc[0:50,1])
 setosa_pl_stdd = np.std(pd_iris_data.iloc[0:50,2])
@@ -134,30 +138,35 @@ setosa_pw_stdd = np.std(pd_iris_data.iloc[0:50,3])
 # Versicolor
 
 # Versicolor Mean
+# Source: 4) np Mean
 vs_sl_mean = np.mean(pd_iris_data.iloc[50:100,0])
 vs_sw_mean = np.mean(pd_iris_data.iloc[50:100,1])
 vs_pl_mean = np.mean(pd_iris_data.iloc[50:100,2])
 vs_pw_mean = np.mean(pd_iris_data.iloc[50:100,3])
 
 # Versicolor Median
+# Source: 5) np Median
 vs_sl_median = np.median(pd_iris_data.iloc[50:100,0])
 vs_sw_median = np.median(pd_iris_data.iloc[50:100,1])
 vs_pl_median = np.median(pd_iris_data.iloc[50:100,2])
 vs_pw_median = np.median(pd_iris_data.iloc[50:100,3])
 
 # Versicolor Minimum
+# Source: 6) np Min 
 vs_sl_min= np.min(pd_iris_data.iloc[50:100,0])
 vs_sw_min= np.min(pd_iris_data.iloc[50:100,1])
 vs_pl_min= np.min(pd_iris_data.iloc[50:100,2])
 vs_pw_min= np.min(pd_iris_data.iloc[50:100,3])
 
 # Versicolor Maximum 
+# Source: 7) np Max
 vs_sl_max = np.max(pd_iris_data.iloc[50:100,0])
 vs_sw_max = np.max(pd_iris_data.iloc[50:100,1])
 vs_pl_max = np.max(pd_iris_data.iloc[50:100,2])
 vs_pw_max = np.max(pd_iris_data.iloc[50:100,3])
 
 # Versicolor Standard Deviation
+# Source: 8) Std Dev
 vs_sl_stdd = np.std(pd_iris_data.iloc[50:100,0])
 vs_sw_stdd = np.std(pd_iris_data.iloc[50:100,1])
 vs_pl_stdd = np.std(pd_iris_data.iloc[50:100,2])
@@ -171,30 +180,35 @@ vs_pw_stdd = np.std(pd_iris_data.iloc[50:100,3])
 # Virginica
 
 # Virginica Mean
+# Source: 4) np Mean
 va_sl_mean = np.mean(pd_iris_data.iloc[100:150,0])
 va_sw_mean = np.mean(pd_iris_data.iloc[100:150,1])
 va_pl_mean = np.mean(pd_iris_data.iloc[100:150,2])
 va_pw_mean = np.mean(pd_iris_data.iloc[100:150,3])
 
 # Virginica Median
+# Source: 5) np Median
 va_sl_median = np.median(pd_iris_data.iloc[100:150,0])
 va_sw_median = np.median(pd_iris_data.iloc[100:150,1])
 va_pl_median = np.median(pd_iris_data.iloc[100:150,2])
 va_pw_median = np.median(pd_iris_data.iloc[100:150,3])
 
 # Virginica Minimum
+# Source: 6) np Min 
 va_sl_min = np.min(pd_iris_data.iloc[100:150,0])
 va_sw_min = np.min(pd_iris_data.iloc[100:150,1])
 va_pl_min = np.min(pd_iris_data.iloc[100:150,2])
 va_pw_min = np.min(pd_iris_data.iloc[100:150,3])
 
 # Virginica Maximum 
+# Source: 7) np Max
 va_sl_max = np.max(pd_iris_data.iloc[100:150,0])
 va_sw_max = np.max(pd_iris_data.iloc[100:150,1])
 va_pl_max = np.max(pd_iris_data.iloc[100:150,2])
 va_pw_max = np.max(pd_iris_data.iloc[100:150,3])
 
 # Virginica Standard Deviation
+# Source: 8) Std Dev
 va_sl_stdd = np.std(pd_iris_data.iloc[100:150,0])
 va_sw_stdd = np.std(pd_iris_data.iloc[100:150,1])
 va_pl_stdd = np.std(pd_iris_data.iloc[100:150,2])
@@ -203,6 +217,10 @@ va_pw_stdd = np.std(pd_iris_data.iloc[100:150,3])
 #end region 
 
 # region DataFrame
+
+# ---------- Creating a DataFrame to show in the summary txt file ----------
+# Source: 9) pdDataFrame
+
 summary = {
     "Feature": ["Overall Data", "Sepal Length", "Sepal Width", "Petal Length", "Petal Width", "Setosa Data", "Setosa Sepal Length", "Setosa Sepal Width", "Setosa Petal Length", "Setosa Petal Width", "Versicolor Data", "Versicolor Sepal Length", "Versicolor Sepal Width", "Versicolor Petal Length", "Versicolor Petal Width", "Virginica Data", "Virginica Sepal Length", "Virginica Sepal Width", "Virginica Petal Length", "Virginica Petal Width"], 
     "Mean": [0, sl_mean, sw_mean, pl_mean, pw_mean, 0, setosa_sl_mean, setosa_sw_mean, setosa_pl_mean, setosa_pw_mean, 0, vs_sl_mean, vs_sw_mean, vs_pl_mean, vs_pw_mean, 0, va_sl_mean, va_sw_mean, va_pl_mean, va_pw_mean],
@@ -215,7 +233,7 @@ summary = {
 # Creating the DataFrame of the summary data
 summary_df = pd.DataFrame(summary)
 
-# Source: https://stackoverflow.com/questions/38255796/pandas-round-is-not-working-for-dataframe
+# Source: 10) dfround
 summary_df['Mean'] = summary_df['Mean'].astype(float).round(1)
 summary_df['Median'] = summary_df['Median'].astype(float).round(1)
 summary_df['Standard Deviation'] = summary_df['Standard Deviation'].astype(float).round(1)
@@ -229,7 +247,7 @@ features_file.write("This file is a summary of the four features: sepal length, 
 features_file.write(f'\nThere is a breakdown of each feature by four categories: total, setosa, versicolor and virginica.')
 features_file.write(f'\nPlease note that a divider dud column has been included to break down the sections e.g. Overall Data which is listed as 0.0 under each measurement')
 features_file.write(f'\nThere are five columns of measurement: Mean (the average), Median (the middle of the data), Minimum (the lowest figure), Maximum (the highest figure) and the Standard Deviation (How much variation there is in a set of data points)')
-features_file.write(f'\nSource for Standard Deviation definition: (https://www.geeksforgeeks.org/standard-deviation-formula/)')
+features_file.write(f'\nSource: 11) Standard Deviation definition: (https://www.geeksforgeeks.org/standard-deviation-formula/)')
 
 features_file.write(f'\n\nThe median, min and max of each variable is plotted on the boxplots, please see the boxplot_outputs folder.\n')
 features_file.write(f'\n\n{summary_df}')
@@ -633,6 +651,7 @@ colours =('#7c70cb')
 plt.grid(linewidth = 0.5, c = 'grey', alpha = 0.2)
 
 # plotting the histogram using sepal_length data, with 10 bins(or bars)
+# Source: edgecolour: (https://matplotlib.org/stable/api/figure_api.html)
 plt.hist(sepal_length, bins = 10, edgecolor = 'black', color = colours)
 
 # Source: https://dnmtechs.com/fixing-deprecationwarning-invalid-escape-sequence-in-python-3/
@@ -666,6 +685,7 @@ colours =('#632AC5')
 plt.grid(linewidth = 0.5, c = 'grey', alpha = 0.2)
 
 # plotting the histogram using sepal_width data, with 10 bins(or bars)
+# Source: # edgecolour: (https://matplotlib.org/stable/api/figure_api.html)
 plt.hist(sepal_width, bins = 10, edgecolor = 'black', color = colours)
 
 # Source: https://dnmtechs.com/fixing-deprecationwarning-invalid-escape-sequence-in-python-3/
@@ -936,3 +956,23 @@ plt.savefig('histogram_outputs/petal_width_species_comparison', bbox_inches = 't
 plt.close()
 
 #endregion
+
+# region References:
+
+# 1) Setting regions: Stack overflow (https://stackoverflow.com/questions/8265583/dividing-python-module-into-multiple-regions)
+# 2) Iris dataset: GitHub (https://gist.githubusercontent.com/curran/a08a1080b88344b0c8a7/raw/0e7a9b0a5d22642a06d3d5b9bcbad9890c8ee534/iris.csv)
+# 3) Creating txt file: W3 Schools (https://w3schools.tech/tutorial/python/python_write_to_file)
+# 4) np Mean: Numpy (https://numpy.org/doc/stable/reference/generated/numpy.mean.html)
+# 5) np Median: Numpy (https://numpy.org/doc/stable/reference/generated/numpy.median.html)
+# 6) np Min: Numpy (https://numpy.org/doc/stable//reference/generated/numpy.min.html)
+# 7) np Max: Numpy (https://numpy.org/doc/stable//reference/generated/numpy.max.html)
+# 8) np Std Dev: Numpy (https://numpy.org/doc/stable/reference/generated/numpy.std.html)
+# 9) pdDataFrame: Geeksforgeeks (https://www.geeksforgeeks.org/how-to-make-a-table-in-python/#using-prettytable)
+# 10) dfround: Stack overflow (https://stackoverflow.com/questions/38255796/pandas-round-is-not-working-for-dataframe)
+# 11) Standard Deviation definition: Geeksforgeeks(https://www.geeksforgeeks.org/standard-deviation-formula/)
+
+# Setting the edgecolour: (https://matplotlib.org/stable/api/figure_api.html)
+
+#endregion
+
+# ---------- END ---------
